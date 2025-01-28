5a92c0f1301e917a992c13fc63b36eb10e77b96f
Kaustubh-Kaka <kaustubhkaka@gmail.com>
2025-01-28 15:00:36 +0530

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,5 +1,414 @@
 let contentID = "TRS";
 
+let trsData = [
+  {
+    name: "Shreyas Anand",
+    year: 1,
+    department: "EE",
+    hall: "LBS",
+    img_src: "shreyas_anand.png",
+  },
+  {
+    name: "Vijeta V Menon",
+    year: 1,
+    department: "CH",
+    hall: "SNVH",
+    img_src: "vijeta_v_menon.png",
+  },
+  {
+    name: "Rupesh Devata",
+    year: 1,
+    department: "MF",
+    hall: "LBS",
+    img_src: "rupesh_devata.png",
+  },
+  {
+    name: "Siddhant Barpanda",
+    year: 1,
+    department: "HS",
+    hall: "LBS",
+    img_src: "siddhant_barpanda.png",
+  },
+  {
+    name: "Ayush Bhushan",
+    year: 1,
+    department: "EE",
+    hall: "LBS",
+    img_src: "ayush_bhushan.png",
+  },
+  {
+    name: "Vinay A",
+    year: 1,
+    department: "EE",
+    hall: "LBS",
+    img_src: "vinay_a.png",
+  },
+  {
+    name: "Siddharth Jain",
+    year: 1,
+    department: "MT",
+    hall: "LBS",
+    img_src: "siddharth_jain.png",
+  },
+  {
+    name: "Vrinda Singh",
+    year: 1,
+    department: "MI",
+    hall: "SNVH",
+    img_src: "vrinda_singh.png",
+  },
+  {
+    name: "Uddipto Mandal",
+    year: 1,
+    department: "IM",
+    hall: "LBS",
+    img_src: "uddipto_mandal.png",
+  },
+  {
+    name: "Md Musheer Khan",
+    year: 1,
+    department: "MT",
+    hall: "LBS",
+    img_src: "md_musheer_khan.png",
+  },
+  {
+    name: "Prakhar Jha",
+    year: 1,
+    department: "IE",
+    hall: "LBS",
+    img_src: "prakhar_jha.png",
+  },
+  {
+    name: "Prateek Kesavarapu",
+    year: 1,
+    department: "NA",
+    hall: "LBS",
+    img_src: "prateek_kesavarapu.png",
+  },
+  {
+    name: "Adya Singh",
+    year: 1,
+    department: "AE",
+    hall: "SNVH",
+    img_src: "adya_singh.png",
+  },
+  {
+    name: "Sujal Singh",
+    year: 1,
+    department: "ME",
+    hall: "LBS",
+    img_src: "sujal_singh.png",
+  },
+  {
+    name: "Sohan Choudhury",
+    year: 1,
+    department: "EC",
+    hall: "LBS",
+    img_src: "sohan_choudhury.png",
+  },
+  {
+    name: "Kaustubh Kaka",
+    year: 1,
+    department: "MA",
+    hall: "LBS",
+    img_src: "kaustubh_kaka.png",
+  },
+  {
+    name: "Siddharth Sreejith",
+    year: 1,
+    department: "EC",
+    hall: "LBS",
+    img_src: "siddharth_sreejith.png",
+  },
+  {
+    name: "Raghav Pande",
+    year: 1,
+    department: "IE",
+    hall: "LBS",
+    img_src: "raghav_pande.png",
+  },
+  {
+    name: "Praneet Dixit",
+    year: 1,
+    department: "ME",
+    hall: "LBS",
+    img_src: "praneet_dixit.png",
+  },
+  {
+    name: "Durva Daga",
+    year: 1,
+    department: "AI",
+    hall: "SNVH",
+    img_src: "durva_daga.png",
+  },
+  {
+    name: "Allu Gowri Sasaank",
+    year: 1,
+    department: "CS",
+    hall: "LBS",
+    img_src: "allu_gowri_sasaank.png",
+  },
+  {
+    name: "Parth Raut",
+    year: 1,
+    department: "ME",
+    hall: "LBS",
+    img_src: "parth_raut.png",
+  },
+  {
+    name: "Aditya Gupta",
+    year: 2,
+    department: "ME",
+    hall: "HJB",
+    img_src: "aditya_gupta.png",
+  },
+  {
+    name: "Anjali Deepu",
+    year: 2,
+    department: "NA",
+    hall: "MT",
+    img_src: "anjali_deepu.png",
+  },
+  {
+    name: "Dhruv Mathur",
+    year: 2,
+    department: "IM",
+    hall: "Patel",
+    img_src: "dhruv_mathur.png",
+  },
+  {
+    name: "Divyanshi Narware",
+    year: 2,
+    department: "ME",
+    hall: "MT",
+    img_src: "divyanshi_narware.png",
+  },
+  {
+    name: "Ishan Phanse",
+    year: 2,
+    department: "AE",
+    hall: "Azad",
+    img_src: "ishan_phanse.png",
+  },
+  {
+    name: "Jash Mehta",
+    year: 2,
+    department: "IM",
+    hall: "MMM",
+    img_src: "jash_mehta.png",
+  },
+  {
+    name: "John Felix",
+    year: 2,
+    department: "IM",
+    hall: "RP",
+    img_src: "john_felix.png",
+  },
+  {
+    name: "Manish Kundu",
+    year: 2,
+    department: "MA",
+    hall: "RK",
+    img_src: "manish_kundu.png",
+  },
+  {
+    name: "Rishith Susarla",
+    year: 2,
+    department: "EC",
+    hall: "VS",
+    img_src: "rishith_susarla.png",
+  },
+  {
+    name: "Samarth G",
+    year: 2,
+    department: "EC",
+    hall: "Azad",
+    img_src: "samarth_g.png",
+  },
+  {
+    name: "Shrreya Rajneesh",
+    year: 2,
+    department: "AE",
+    hall: "SNVH",
+    img_src: "shrreya_rajneesh.png",
+  },
+  {
+    name: "Divyansh Sharma",
+    year: 3,
+    department: "GG",
+    hall: "RP",
+    img_src: "divyansh_sharma.png",
+  },
+  {
+    name: "Meera Nair",
+    year: 3,
+    department: "ME",
+    hall: "MT",
+    img_src: "meera_nair.png",
+  },
+  {
+    name: "Muskan Khemka",
+    year: 3,
+    department: "GG",
+    hall: "SN/IG",
+    img_src: "muskan_khemka.png",
+  },
+  {
+    name: "Parag Agarwal",
+    year: 3,
+    department: "IM",
+    hall: "MMM",
+    img_src: "parag_agarwal.png",
+  },
+  {
+    name: "Prakhar Tripathi",
+    year: 3,
+    department: "MI",
+    hall: "RP",
+    img_src: "prakhar_tripathi.png",
+  },
+  {
+    name: "Pranav Mangal Pandey",
+    year: 3,
+    department: "CE",
+    hall: "MMM",
+    img_src: "pranav_mangal_pandey.png",
+  },
+  {
+    name: "Swaminathan S K",
+    year: 3,
+    department: "CS",
+    hall: "HJB",
+    img_src: "swaminathan_sk.png",
+  },
+  {
+    name: "U Skanda Aithal",
+    year: 3,
+    department: "EC",
+    hall: "Azad",
+    img_src: "u_skanda_aithal.png",
+  },
+  {
+    name: "Abhinav Viju Pillai",
+    year: 4,
+    department: "ME",
+    hall: "RK",
+    img_src: "abhinav_viju_pillai.png",
+  },
+  {
+    name: "Aditya Nandy",
+    year: 4,
+    department: "EE",
+    hall: "Patel",
+    img_src: "aditya_nandy.png",
+  },
+  {
+    name: "Akshay S Menon",
+    year: 4,
+    department: "EE",
+    hall: "MMM",
+    img_src: "akshay_s_menon.png",
+  },
+  {
+    name: "Amrit Lal Singh",
+    year: 4,
+    department: "IM",
+    hall: "LBS",
+    img_src: "amrit_lal_singh.png",
+  },
+  {
+    name: "Parth Gupta",
+    year: 4,
+    department: "BT",
+    hall: "Patel",
+    img_src: "parth_gupta.png",
+  },
+  {
+    name: "Parthivi Gupta",
+    year: 4,
+    department: "AE",
+    hall: "SNVH",
+    img_src: "parthivi_gupta.png",
+  },
+  {
+    name: "Prasanna Paithankar",
+    year: 4,
+    department: "CS",
+    hall: "Azad",
+    img_src: "prasanna_paithankar.png",
+  },
+  {
+    name: "Siddharth Asthana",
+    year: 4,
+    department: "MI",
+    hall: "RK",
+    img_src: "siddharth_asthana.png",
+  },
+  {
+    name: "Tanushree Prasad",
+    year: 4,
+    department: "ME",
+    hall: "MT",
+    img_src: "tanushree_prasad.png",
+  },
+  {
+    name: "Vivek Mirani",
+    year: 4,
+    department: "ME",
+    hall: "RP",
+    img_src: "vivek_mirani.png",
+  },
+  {
+    name: "Harmeet singh",
+    year: 5,
+    department: "CY",
+    hall: "Azad",
+    img_src: "harmeet_singh.png",
+  },
+  {
+    name: "Yashi Gaur",
+    year: 5,
+    department: "ME",
+    hall: "SN/IG",
+    img_src: "yashi_gaur.png",
+  },
+];
+
+let toggleState = Array(trsData.length).fill(true);
+
+let yearState = [false, true, true, true, true];
+
+function yearToRole(n) {
+  if (n == 1) return "Member";
+  else if (n == 2) return "Sub-Head";
+  else if (n == 3) return "Head";
+  else return "Coordinator";
+}
+
+function cardHTML(obj, hideData = false) {
+  if (yearState[obj.year - 1])
+    return `
+        <div class="namecard">
+          <div class="Image-wrapper"><img src="./trsimages/${
+            obj.img_src
+          }"></div>
+          <p class="Name">${obj.name}</p>
+          <p class="Hall">${obj.hall}</p>
+          <p class="Department">${obj.department}</p>
+          <p class="Role">${yearToRole(obj.year)}</p>
+        </div>
+      `;
+  else return "";
+}
+
+function renderAll() {
+  for (let i = 0; i < trsData.length; i++)
+    document.querySelector("#card-wrapper").innerHTML += cardHTML(
+      trsData[i],
+      !toggleState[i]
+    );
+}
+
 function renderMainContent() {
   if (contentID == "animation") {
     document.querySelector("#content-area").innerHTML = document.querySelector(
@@ -331,9 +740,8 @@ function renderMainContent() {
   } else if (contentID == "blog") {
     document.querySelector("#content-area").innerHTML = ``;
   } else if (contentID == "TRS") {
-    document.querySelector("#content-area").innerHTML = document.querySelector(
-      "#content-area"
-    ).innerHTML = document.querySelector("#TRS-content").innerHTML;
+    // card rendering logic
+    renderAll();
   }
 }
 
